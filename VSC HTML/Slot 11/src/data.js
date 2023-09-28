3e4430cb22d416863af3132423f622d9767ad208
lantdth2303014 <133187878+lantdth2303014@users.noreply.github.com>
2023-09-28 17:56:50 +0700

    Add files via upload

---
+++ VSC HTML/Slot 11/src/data.js
@@ -0,0 +1,71 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "Samsung Galaxy S23 Ultra",
+    "price": 1199,
+    "description": "6.8-inch display<br> 200MP camera<br> 12GB RAM<br> 5000mAh battery",
+    "brand": "Samsung",
+    "image": "images/samsung1.png"
+}, {
+    "id": 2,
+    "title": "iPhone 14 Pro Max 6GB 128GB",
+    "price": 1099,
+    "description": "6.7-inch display<br> 48MP camera<br> 6GB RAM<br> 4323mAh battery",
+    "brand": "Apple",
+    "image": "images/apple1.png"
+}, {
+    "id": 3,
+    "title": "Xiaomi 13 Ultra 12GB 256GB",
+    "price": 913,
+    "description": "6.36-inch display<br> 50MP camera<br> 12GB RAM<br> 7538mAh battery",
+    "brand": "Xiaomi",
+    "image": "images/xiaomi1.png"
+}, {
+    "id": 4,
+    "title": "Samsung Galaxy Tab S8 Wifi",
+    "price": 755,
+    "description": "11-inch display<br> 13MP camera<br> 8GB RAM<br> 8000mAh battery",
+    "brand": "Samsung",
+    "image": "images/samsung2.png"
+}, {
+    "id": 5,
+    "title": "iPad Pro 11-inch 2022 M2 Wifi 128GB",
+    "price": 999,
+    "description": "11-inch display<br> 12MP camera<br> 8GB RAM<br> 7538mAh battery",
+    "brand": "Apple",
+    "image": "images/apple2.png"
+}, {
+    "id": 6,
+    "title": "Xiaomi Pad 6 Pro Wifi 8GB 128GB",
+    "price": 563,
+    "description": "11-inch display<br> 13MP camera<br> 6GB RAM<br> 8840mAh battery",
+    "brand": "Xiaomi",
+    "image": "images/xiaomi2.png"
+}, {
+    "id": 7,
+    "title": "Samsung Galaxy Watch 4 40mm",
+    "price": 399,
+    "description": "1.5-inch display<br> heart rate monitor<br> 2GB RAM<br> 425mAh battery",
+    "brand": "Samsung",
+    "image": "images/samsung3.png"
+}, {
+    "id": 8,
+    "title": "Apple Watch Series 8 Aluminum",
+    "price": 340,
+    "description": "1.9-inch display<br> heart rate monitor<br> 1GB RAM<br> 308mAh battery",
+    "brand": "Apple",
+    "image": "images/apple3.png"
+}, {
+    "id": 9,
+    "title": "Xiaomi Watch S1 Active Pro Steel",
+    "price": 145,
+    "description": "1.43-inch display<br> heart rate monitor<br> 1GB RAM<br> 470mAh battery",
+    "brand": "Xiaomi",
+    "image": "images/xiaomi3.png"
+}]
